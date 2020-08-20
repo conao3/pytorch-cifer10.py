b88e8a2dd559a803a832bed06d23260efbcce8a6
conao3 <conao3@gmail.com>
2020-08-20 15:42:20 +0900

    commit

--- pytorch-cifer10.py
+++ pytorch-cifer10.py
@@ -4,6 +4,7 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.utils.tensorboard as tfutil
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -24,16 +25,18 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(3, 6, 5)   # 32x32x3 -> 10x10x6
+        self.pool = nn.MaxPool2d(2, 2)    # 10x10x16 -> 5x5x16
+        self.conv2 = nn.Conv2d(6, 16, 5)  # 10x10x6 -> 10x10x16
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        # x = self.pool(F.relu(self.conv1(x)))
+        # x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.pool(self.conv1(x)))
+        x = F.relu(self.pool(self.conv2(x)))
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -45,30 +48,61 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 net = Net().to(device)
 
-criterion = nn.CrossEntropyLoss()
+# criterion = nn.CrossEntropyLoss()
+criterion = nn.CosineEmbeddingLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-for epoch in range(5):  # loop over the dataset multiple times
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
+# "./logs/pool2-right-celoss-adam-lr0_001"
+with tfutil.SummaryWriter("./logs/cosEmbLoss-adam-lr0_001-momentum0_9") as writer:
+    for epoch in range(6):
+        running_loss = 0.0
+        traintotal, traincorrect = 0, 0
+        for i, data in enumerate(trainloader):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+            _, trainpredicted = torch.max(outputs.data, 1)
+            traintotal += labels.size(0)
+            traincorrect += (trainpredicted == labels).sum().item()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                x, y = epoch*12000+i, running_loss/2000
+                print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, y))
+                writer.add_scalar("loss/train", y, x)
+                writer.add_scalar("accuracy/train", 100 *
+                                  traincorrect/traintotal, x)
+                running_loss = 0.0
+                traintotal, traincorrect = 0, 0
+
+                with torch.no_grad():
+                    testtotal, testcorrect = 0, 0
+                    for testdata in testloader:
+                        testimages, testlabels = testdata
+                        testimages, testlabels = testimages.to(
+                            device), testlabels.to(device)
+                        testoutputs = net(testimages)
+                        testloss = criterion(testoutputs, testlabels)
+
+                        _, predicted = torch.max(testoutputs.data, 1)
+                        testtotal += testlabels.size(0)
+                        testcorrect += (predicted == testlabels).sum().item()
+
+                    writer.add_scalar("accuracy/varidation", 100 *
+                                      testcorrect/testtotal, x)
+                    writer.add_scalar("loss/varidation", testloss.item(), x)
+
 
 print('Finished Training')
 
